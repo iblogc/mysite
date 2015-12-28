ac277e3fbade873aad03c09093e6101b8c24cea1
jfhuang <korvin101@gmail.com>
2015-12-28 22:26:56 +0800

    add python-Markdown

---
+++ static/js/sequence-loader.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Tom on 2015/12/28.
+ */
+/* sequence-loader.js */
+(function (doc) {
+  function onReady(fn) {
+    if (doc.addEventListener) {
+      doc.addEventListener('DOMContentLoaded', fn);
+    } else {
+      doc.attachEvent('onreadystatechange', function() {
+        if (doc.readyState === 'interactive')
+          fn();
+      });
+    }
+  }
+
+  onReady(function(){convertUML('uml-sequence-diagram', Diagram, {theme: 'simple'});});
+})(document)
