45f7a9ef6436fd7eda662f1aef73de4f65e721c0
jfhuang <korvin101@gmail.com>
2015-12-28 22:49:47 +0800

    sdf

--- mysite/settings/__init__.py
+++ mysite/settings/__init__.py
@@ -2,9 +2,12 @@
 from __future__ import unicode_literals
 from __future__ import absolute_import
 import os
+import sys
 
 __author__ = 'hjf'
 
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
 
 if 'SERVER_SOFTWARE' in os.environ:
     from .prod import *
